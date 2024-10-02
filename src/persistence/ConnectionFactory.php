8539bb3306c1b6fa2922423e7b7acaf8f595bb1a
Gustavo Ribeiro <gugzribeiro@gmail.com>
2024-10-02 19:30:49 -0300

    Commits que o Maia fez

--- src/persistence/ConnectionFactory.php
+++ src/persistence/ConnectionFactory.php
@@ -1,24 +1,32 @@
 <?php
 
-    namespace MeuProjeto\persistence;
+namespace MeuProjeto\persistence;
 
 use \PDO;
+use \PDOException;
 
 class ConnectionFactory {
 
     private static $connection = null;
 
-    //Conexão com Banco de Dados MySQL
+    // Conexão com Banco de Dados MySQL
     public static function getConnection() {
-       
         if (self::$connection === null) {
-            $dnsStr = "mysql:host=172.17.0.2;dbname=bdqiplanta";
-            self::$connection = new \PDO($dnsStr, "root", "1234");  
-            self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $host = getenv('DB_HOST') ?: '172.17.0.2';
+            $dbname = getenv('DB_NAME') ?: 'bdqiplantav3';
+            $username = getenv('DB_USER') ?: 'root';
+            $password = getenv('DB_PASS') ?: '1234';
+            $dsn = "mysql:host=$host;dbname=$dbname";
+
+            try {
+                self::$connection = new PDO($dsn, $username, $password);
+                self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                // Log the error message or handle it as needed
+                die('Connection failed: ' . $e->getMessage());
+            }
         }
 
         return self::$connection;
-
-    }    
-
+    }
 }
