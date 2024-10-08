fb870b2d375dc23d125a586929ed55f0d3e06bad
O Famosoo Pedro <joaopedropaulohenrique@gmail.com>
2024-10-08 02:08:50 -0300

    pequenas alterações nas paginas & codigos
    pequenas alterações:
    .css
    - telacadastro

--- src/views/TelaCadastro.php
+++ src/views/TelaCadastro.php
@@ -9,54 +9,62 @@
 <body>
     <header>
     <div class="logo">
-            <a href="index.php"><img src="./images/QiPlanta.png" alt="Logo do Marketplace"></a> <!-- Substitua pelo caminho do seu logo -->
-        </div>
+        <a href="index.php"><img src="./images/QiPlanta.png" alt="Logo do Marketplace"></a> <!-- Substitua pelo caminho do seu logo -->
+    </div>
         <nav>
             <ul>
                 <li><a href="./../index.php">Login</a></li>
-                <li><a href="produtos.php">Produtos</a></li>
-
+                <li><a href="menu.php">Home</a></li>
             </ul>
         </nav>
+</header>
+<main>
+    <div class="container-cadastro">
+        <div class="divisoria-cadastro">
+            <div>
 
-    </header>
-    <main>
-        <div class="container-cadastro">
                 <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                <h1>Cadastro de Cliente </h1><br>           
-                    
-                    <label for="username">Nome de Usuário:</label><br>
-                    <input class="form-input" type="text" id="username" name="nome" placeholder="Insira um nome de usuário" required><br><br>
-                    
-                    <label for="username">Endereço:</label><br>
-                    <input class="form-input" type="text" id="endereco" name="endereco" placeholder="Digite o seu Endereço" required><br><br>
+                    <h1>Cadastro de Cliente </h1><br>           
                 
-                    <label for="username">Email:</label><br>
-                    <input class="form-input" type="text" id="email" name="email" placeholder="Insira o seu e-mail" required><br><br>
+                <label for="username">Nome de Usuário:</label><br>
+                <input class="form-input" type="text" id="username" name="nome" placeholder="Insira um nome de usuário" required><br><br>
                 
-                    <label for="username">Numero do Celular:</label><br>
-                    <input class="form-input" type="number" id="numcell" name="numcell" placeholder="(xx) xxx xxx xxx" required><br><br>
-
-                    <label for="password">Senha:</label><br>
-                    <input class="form-input" type="password" id="password" name="senha" placeholder="Insira uma senha" required>
-
-                    <label for="password">Repita sua senha:</label><br>
-                    <input class="form-input" type="password" id="password" name="senha2" placeholder="Confirme sua senha" required><br><br>
-                        
-                    <input type="checkbox"name="robot" id="robot" required>
-                    
-                    <label>Não sou um robo</label>
-                    
-                        <div style="left:10px;">
-                        <button class="cadastro-btn" id="btn-cadastro" type="submit" value="Cadastrar">Cadastrar</button>
-
-
-                        </div>
-
+                <label for="username">Endereço:</label><br>
+                <input class="form-input" type="text" id="endereco" name="endereco" placeholder="Digite o seu Endereço" required><br><br>
+                
+                <label for="username">Email:</label><br>
+                <input class="form-input" type="text" id="email" name="email" placeholder="Insira o seu e-mail" required><br><br>
+                
+                <label for="username">Numero do Celular:</label><br>
+                <input class="form-input" type="number" id="numcell" name="numcell" placeholder="(xx) xxx xxx xxx" required><br><br>
+                
+                <label for="password">Senha:</label><br>
+                <input class="form-input" type="password" id="password" name="senha" placeholder="Insira uma senha" required>
+                
+                <label for="password">Repita sua senha:</label><br>
+                <input class="form-input" type="password" id="password" name="senha2" placeholder="Confirme sua senha" required><br><br>
+                
+                <input type="checkbox"name="robot" id="robot" required>
+                <label>Não sou um robo</label>
+                
+                <div style="left:10px;">
+                    <button class="cadastro-btn" id="btn-cadastro" type="submit" value="Cadastrar">Cadastrar</button>
+                </div>
             </form>
+           </div>
+           <div class="divisoria-cadastro-login">
+                <center>
+                    <img src="./images/QiPlanta.png" alt="Logo do Marketplace" style="height: 200px; margin-top: 40px;">
+                        <p>Ja possui uma conta? <br></p> <br>
+                        <button type="button" href="./../index.php">Login</button>
+                </center>
+           </div>
         </div>
-    </main>
-
+    </div>
+</main>
+<footer>
+    <?php include("footer.php") ?>
+</footer>
     
     
     <?php
