a7b2fb41d091e8e7979c2c21593421f4649a96ae
EmbryND <elvislimasamuel27@gmail.com>
2024-10-16 20:46:43 -0300

    Implementação de algumas informações no arquivo 'sobre.php'

---
+++ src/views/sobre.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre nós</title>
+</head>
+<body>
+    <h1>Lorem Ipsum PT. 1</h1>
+    <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor sapien erat, sit amet posuere sem finibus in. Nunc ultrices orci non sem laoreet, a porta eros dapibus. Aliquam sit amet nulla pellentesque, blandit magna eget, rhoncus nisl. Fusce molestie congue mauris, eget venenatis dolor. Maecenas sagittis pulvinar maximus. Donec in augue in ipsum ullamcorper varius a eu diam. Ut et efficitur ex. Nunc ultricies lacus porttitor, rhoncus lectus id, fermentum arcu. Integer interdum magna et vestibulum accumsan.
+
+Morbi purus ligula, dignissim eu ligula quis, finibus aliquam ligula. Donec massa ex, molestie vitae massa in, volutpat condimentum ligula. Etiam nisl risus, imperdiet consectetur rhoncus venenatis, fringilla at turpis. Vivamus consectetur, eros a congue volutpat, velit ipsum elementum velit, consequat ultricies massa libero ut urna. Aliquam erat volutpat. Donec a tortor vel arcu tristique malesuada. Maecenas pellentesque quam non magna dictum posuere. Vivamus accumsan facilisis malesuada. Nunc fringilla lectus magna, ut sollicitudin neque finibus a. Pellentesque tellus magna, tristique ac elit et, sodales blandit mauris. Sed dapibus, urna non suscipit sollicitudin, sem magna congue neque, sed tempus ipsum risus at massa. In et volutpat nisl. Vestibulum rutrum, magna id tristique aliquet, neque ante gravida neque, fringilla consectetur nisi nunc non sapien.</p>
+<h1>Lorem Ipsum PT. 2</h1>
+<p>Morbi purus ligula, dignissim eu ligula quis, finibus aliquam ligula. Donec massa ex, molestie vitae massa in, volutpat condimentum ligula. Etiam nisl risus, imperdiet consectetur rhoncus venenatis, fringilla at turpis. Vivamus consectetur, eros a congue volutpat, velit ipsum elementum velit, consequat ultricies massa libero ut urna. Aliquam erat volutpat. Donec a tortor vel arcu tristique malesuada. Maecenas pellentesque quam non magna dictum posuere. Vivamus accumsan facilisis malesuada. Nunc fringilla lectus magna, ut sollicitudin neque finibus a. Pellentesque tellus magna, tristique ac elit et, sodales blandit mauris. Sed dapibus, urna non suscipit sollicitudin, sem magna congue neque, sed tempus ipsum risus at massa. In et volutpat nisl. Vestibulum rutrum, magna id tristique aliquet, neque ante gravida neque, fringilla consectetur nisi nunc non sapien.</p>
+</body>
+</html>
