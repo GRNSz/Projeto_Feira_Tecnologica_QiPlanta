c801b890d98cded3c993596f9510c1a8a5099302
EmbryND <elvislimasamuel27@gmail.com>
2024-10-17 21:34:31 -0300

    Expansão do 'Quem somos?' e 'Qual o nosso objetivo?'

--- src/views/sobre.php
+++ src/views/sobre.php
@@ -15,10 +15,16 @@
     <main>
         <h1>Quem somos?</h1>
         <p>
-            Somos um grupo que visa espalhar e consicientizar outros indivíduos sobre a importância que a fauna possui para a população mundial. <!-- Fiquei sem criatividade, mas caso queira implementar algo a mais, fique a vontade. -->
+        Somos um grupo que visa espalhar e consicientizar outros indivíduos sobre a importância que a fauna possui para a população mundial. <br />
+        Em um mundo onde há a crescente da poluição atmosférica, poluição marinha e o aumento do aquecimento global, surge a necessidade da conscientização <br />
+        sobre os perigos que recursos poluentes trazem para a saúde humana e para o meio ambiente.
         </p>
         <h1>Qual o nosso objetivo?</h1>
-        <p>Nosso objetivo é propagar o conhecimento sobre plantas e conscientizar o povo a respeito da plantação de árvores no meio-ambiente.</p>
+        <p>
+            Nosso objetivo é espalhar o conhecimento sobre plantas e conscientizar o povo a respeito da plantação de árvores no meio-ambiente. <br />
+            Aqui, vendemos plantas e explicamos suas características acerca do mesmo, além de citar curiosidades interessantes sobre algumas plantas <br />
+            que talvez algumas pessoas não saibam. Além disso, postamos blogs sobre o prejuízo que está sendo afetado no meio-ambiente e detalhamos sobre.
+        </p>
         <h1>Nosso trajeto</h1>
         <p>Começamos apenas como uma simples tela de login feito em PHP, mas nosso projeto evoluiu-se para um projeto para a feira tecnológica da Etec.</p>
     </main>
