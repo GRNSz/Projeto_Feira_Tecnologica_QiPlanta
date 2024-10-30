14e45db407725eca9f6338c6ee530265a790a70b
Gustavo Ribeiro <gugzribeiro@gmail.com>
2024-10-30 15:25:38 -0300

    Teste

--- src/main.php
+++ src/main.php
@@ -13,7 +13,3 @@ use MeuProjeto\model\Login;
 use MeuProjeto\configuration\ConnectionFactory;
 
 // Cria uma instância da classe Login
-$login = new Login($arg1, $arg2); // Replace $arg1 and $arg2 with the actual arguments required by the Login constructor
-
-$connection= new ConnectionFactory();   
-$connection = ConnectionFactory::getConnection();
