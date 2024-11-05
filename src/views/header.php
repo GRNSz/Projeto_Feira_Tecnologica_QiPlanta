bdcee68b141b443b687249413c96066e49893efd
O Famosoo Pedro <joaopedropaulohenrique@gmail.com>
2024-11-05 01:39:41 -0300

    remoção do bug do dropdown

--- src/views/header.php
+++ src/views/header.php
@@ -21,19 +21,12 @@
             </a>
         </li>
         <li>
-            <div class="dropdown">
-                <a href="./../../src/views/categorias.php">
+            <a href="./../../src/views/categorias.php">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">  
                         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>    
                     </svg>
                 Categorias
-                </a>
-            <ul>
-                <li><a href="./../../src/views/menu.php#plantas">Plantas</a></li>
-                <li><a href="#">Categoria 2</a></li>
-                <li><a href="#">Categoria 3</a></li>
-            </ul>
-            </div>
+            </a>
         </li>
 
 
--- src/views/sobre.php
+++ src/views/sobre.php
@@ -6,24 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sobre nós</title>
     <link rel="icon" href="./images/icons8-pinheiro-162.png" type="image/png">
+    <link rel="stylesheet" href="./css/menu.css"> <!-- linkado com o css "principal" -->
 </head>
 
 <body>
     <header>
-        <?php include("header.php"); ?>
+        <?php include(__DIR__ ."/header.php"); ?>
     </header>
     <main>
-        <h1>Quem somos?</h1>
-        <p>
-        Somos um grupo que visa espalhar e consicientizar outros indivíduos sobre a importância que a fauna possui para a população mundial. <br />
-        Em um mundo onde há a crescente da poluição atmosférica, poluição marinha e o aumento do aquecimento global, surge a necessidade da conscientização <br />
-        sobre os perigos que recursos poluentes trazem para a saúde humana e para o meio ambiente.
-        </p>
-        <h1>Qual o nosso objetivo?</h1>
-        <p>
-            Nosso objetivo é espalhar o conhecimento acerca de plantas e conscientizar o povo a respeito da plantação de árvores no meio-ambiente. <br />
-            Aqui, vendemos plantas e explicamos suas características acerca do mesmo, além de citar curiosidades interessantes sobre algumas plantas <br />
-            que talvez algumas pessoas não saibam. Além disso, postamos blogs sobre o prejuízo que está sendo afetado no meio-ambiente e detalhamos sobre.
+        <div class="caixa-sobre">
+
+            <h1>Quem somos?</h1>
+            <p>
+                Somos um grupo que visa espalhar e consicientizar outros indivíduos sobre a importância que a fauna possui para a população mundial. <br />
+                Em um mundo onde há a crescente da poluição atmosférica, poluição marinha e o aumento do aquecimento global, surge a necessidade da conscientização <br />
+                sobre os perigos que recursos poluentes trazem para a saúde humana e para o meio ambiente.
+            </p>
+            <h1>Qual o nosso objetivo?</h1>
+            <p>
+                Nosso objetivo é espalhar o conhecimento acerca de plantas e conscientizar o povo a respeito da plantação de árvores no meio-ambiente. <br />
+                Aqui, vendemos plantas e explicamos suas características acerca do mesmo, além de citar curiosidades interessantes sobre algumas plantas <br />
+                que talvez algumas pessoas não saibam. Além disso, postamos blogs sobre o prejuízo que está sendo afetado no meio-ambiente e detalhamos sobre.
         </p>
         <h1>Nosso trajeto</h1>
         <p>
@@ -31,6 +34,7 @@
             Ao longo do caminho, enfretamos desafios e dificuldades no desenvolvimento do projeto, superando obstáculos com o trabalho em equipe e com a <br />
             colaboração da nossa equipe de desenvolvedores.
         </p>
+    </div>
     </main>
     <footer>
         <?php include("./footer.php") ?>
