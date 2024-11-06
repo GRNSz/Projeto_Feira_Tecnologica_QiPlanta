e4d029cb572efd87ac90dbc3ac11bb694d33d010
EmbryND <elvislimasamuel27@gmail.com>
2024-11-06 20:39:15 -0300

    Só um instante...

--- src/controllers/ProdutoController.php
+++ src/controllers/ProdutoController.php
@@ -75,13 +75,7 @@ class ProdutoController {
                 'descricao' => 'A pazinha de jardim é uma ferramenta prática para plantar, transplantar e cuidar de pequenas plantas. Com lâmina resistente e cabo ergonômico, facilita o trabalho em áreas menores e de difícil acesso. Ideal para jardineiros iniciantes e experientes.',
                 'categoria' => 'Ferramentas'
             ],
-            9 => [
-                'nome' => 'Pazinha para Jardim',
-                'preco' => 30.00,
-                'imagem' => './images/pazinha.png',
-                'descricao' => 'A pazinha de jardim é uma ferramenta prática para plantar, transplantar e cuidar de pequenas plantas. Com lâmina resistente e cabo ergonômico, facilita o trabalho em áreas menores e de difícil acesso. Ideal para jardineiros iniciantes e experientes.',
-                'categoria' => 'Ferramentas'
-            ],
+            
         ];
     }
 
