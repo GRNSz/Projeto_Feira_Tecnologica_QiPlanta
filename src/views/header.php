4cf4fffb072375c580e83873661e230a0e541319
Resett13 <pietrogoncalvesaraujo1301@gmail.com>
2024-11-11 19:30:43 -0300

    adicao no nav

--- src/views/header.php
+++ src/views/header.php
@@ -24,9 +24,9 @@
         </li>
         <li>
             <a href="./../../src/views/categorias.php">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-collection-fill" viewBox="0 0 16 16">
+            <img src="../views/images/Categorias.png" width="20px" height="20px">
   <path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3m2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1"/>
-</svg>
+</i>
                 Categorias
             </a>
         </li>
