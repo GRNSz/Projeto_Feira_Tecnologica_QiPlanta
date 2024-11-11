9a01bbc2b00d1826362a0b4c2694efdca3e8ae75
Gustavo Ribeiro <gugzribeiro@gmail.com>
2024-11-11 07:17:46 -0300

    Modificações e exclusões de alguns arquivos para melhor funcionamento

--- src/index.php
+++ src/index.php
@@ -1,10 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="refresh" content="0; url=login.php">
-    <title>Redirecting...</title>
-</head>
-<body>
-    <p>If you are not redirected automatically, follow this <a href="login.php">link to login</a>.</p>
-</body>
-</html>
+<meta http-equiv="refresh" content="0; url=./views/login.php">
