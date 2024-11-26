f0d7717391bf1ded92b3f0c47ed0e7bd3873d704
Matheus Maia <tecmaia7@gmail.com>
2024-11-25 21:42:33 -0300

    API com Controller

---
+++ src/configuration/MercadoPago.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+MercadoPago\SDK::setAccessToken('TEST-2947339509677471-112018-f5b392bbfb8dafb5d160fa6577380fae-2108002326');
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'gusta/projeto_feira_tecnologica_qi-planta',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'dbd54218098693a3d28b72824314fb7c51a6de1b',
+        'reference' => '55f320126d252fb762d6a3714faf9059ac97964c',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'gusta/projeto_feira_tecnologica_qi-planta' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'dbd54218098693a3d28b72824314fb7c51a6de1b',
+            'reference' => '55f320126d252fb762d6a3714faf9059ac97964c',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
